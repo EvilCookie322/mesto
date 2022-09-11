47a08237cd7f2c10a16cde3f701a4e030e11bae6
Cookie <sanches9797@yandex.ru>
2022-09-11 22:44:12 +0300

    fix: fix BEM naming

--- scripts/index.js
+++ scripts/index.js
@@ -12,8 +12,8 @@ const likeButton = document.querySelectorAll('.element__button-like');
 const popupAdd = document.querySelector('.popup-add');
 const placeAddButton = document.querySelector('.profile__add-button');
 const addForm = document.querySelector('.add-form');
-const addElementName = document.querySelector('.add-form__input_el_name');
-const addElementLink = document.querySelector('.add-form__input_el_link')
+const addElementName = document.querySelector('#add-form__name');
+const addElementLink = document.querySelector('#add-form__link');
 
 const elementTemplate = document.querySelector('#element').content;
 const elements = document.querySelector('.elements');
