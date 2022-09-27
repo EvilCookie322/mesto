dc3898e63a8546102c7bdd73fb0d1f02a4089d80
Cookie <sanches9797@yandex.ru>
2022-09-27 13:40:30 +0300

    refact: add class Card

---
+++ scripts/Card.js
@@ -0,0 +1,32 @@
+class Card {
+
+	#name;
+	#link;
+	#template;
+
+	constructor(name, link, template) {
+		this.#name = name;
+		this.#link = link;
+		this.#template = template;
+	}
+
+	#addEventListeners() {
+
+	}
+
+	#handlePreviewCard() {
+
+	}
+
+	#handleDeleteCard() {
+
+	}
+
+	#handleLike() {
+
+	}
+
+	create() {
+
+	}
+}
